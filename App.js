f9c3f45ee0e9ba601ef6d44f65a5a416d70df350
GSoni <giriraj.soni@exelaonline.com>
2024-02-27 15:15:53 +0530

    Added bundler and browserslist for old browser compatiable

--- App.js
+++ App.js
@@ -1,9 +1,14 @@
+import React  from 'react';
+import ReactDOM from 'react-dom/client';
+
+
+
 const parent = React.createElement('div', {
     id: "parent"
 }, [
     React.createElement('div', {
         id: "child"
-    }, [React.createElement('h1', {}, "I am a H1 Tag"), React.createElement('h2', {}, "I am a H2 Tag")]),
+    }, [React.createElement('h1', {}, "This is namaste React"), React.createElement('h2', {}, "I am a H2 Tag")]),
     React.createElement('div', {
         id: "child2"
     }, [React.createElement('h1', {}, "I am H1 Tag from another child"), React.createElement("h2", {}, "I am an H2 Tag from another child")])
--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # Namaste React
+
+
+# Parcel
+- Dev Build
+- Local Server
+- HMR - Hot Module Replacement
+- File Watching Algorthim - written in c++
+- Caching - Faster Builds
+- Image optimization
+- Minfication of file for production build
+- compressing the file
+- Consitent Hashing - It Tracks the versioning of your application
+- Code splitting
+- Differential bundling - support older browsers
+- Diagnostic
+- Error Handling
+- It can give HTTPS server
+- Tree Shaking Algorithim - removed unused code for you
+- Different Build or Bundle for DEV and Production
+
+parcel is bundler
